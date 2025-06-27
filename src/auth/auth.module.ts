c8e1bf5f310b12b185e094eb312a9427f562e86a
SxLegacy <manuel.dev.eng.27@gmail.com>
2025-06-27 15:49:50 -0400

    .env jwt deploying

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,17 +3,27 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants/jwt.constants';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule,
-    JwtModule.register({
-      global: true,
-      secret: jwtConstants.secret, //esto genera un nivel extra de seguridad llamando la palabra clave
-      signOptions: { expiresIn: "1d" }
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        global: true,
+        signOptions: { expiresIn: "1d" },
+      }),
+      inject: [ConfigService],
     }),
+    //   global: true,
+    //   secret: jwtConstants.secret, //esto genera un nivel extra de seguridad llamando la palabra clave
+    //   signOptions: { expiresIn: "1d" }
+    // }),
   ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
+  exports: [JwtModule],
 })
 export class AuthModule {}
